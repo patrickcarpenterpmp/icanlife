bd8fcb11bea4b3e00ba6fe09f1c8846a981f23b9
patrickcarpenterpmp <patrickcarpenterpmp@gmail.com>
2025-12-08 14:08:14 -1000

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint during build
-  },
-}
-
-module.exports = nextConfig
